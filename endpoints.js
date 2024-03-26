24a63fbfd8104b30b485855b8a544b59b203807b
Anne Blankert <anne.blankert@geodan.nl>
2024-03-26 09:42:47 +0100

    some new endpoints

--- endpoints.js
+++ endpoints.js
@@ -1,8 +1,10 @@
 export const cswEndPoints = [
-  {name: "geonorge", url:"https://www.geonorge.no/geonetwork/srv/nor/csw"},
-  {name: "nationaalgeoregister", url: "https://nationaalgeoregister.nl/geonetwork/srv/dut/csw-inspire"},
-  {name: "linz", url: "https://data.linz.govt.nz/services/csw?service=CSW&version=2.0.2&request=GetRecords"},
-  {name: "geo-solutions", url: "https://gs-stable.geo-solutions.it/geoserver/csw"},
-  {name: "geoplatforme.fr", url: "https://data.geopf.fr/csw"},
-  {name: "ign-fr", url: "https://wxs.ign.fr/catalogue/csw-inspire/"}
+  {name: "geonorge", url:"https://www.geonorge.no/geonetwork/srv/nor/csw", language: "Norwegian"},
+  {name: "nationaalgeoregister", url: "https://nationaalgeoregister.nl/geonetwork/srv/dut/csw-inspire", language: "Dutch"},
+  {name: "linz", url: "https://data.linz.govt.nz/services/csw?service=CSW&version=2.0.2&request=GetRecords", language: "English"},
+  {name: "geo-solutions", url: "https://gs-stable.geo-solutions.it/geoserver/csw", language: "Italian"},
+  {name: "geoplatforme.fr", url: "https://data.geopf.fr/csw/", language: "French"},
+  {name: "Bundesamt für Kartographie und Geodäsie (BKG)", url: "https://mis.bkg.bund.de/csw", language: "German"},
+  {name: "ign-fr", url: "https://wxs.ign.fr/catalogue/csw-inspire/", language: "French"},
+  {name: "ign-es", url: "http://www.ign.es/csw-inspire/srv/spa/csw", language: "Spanish"},
 ]
